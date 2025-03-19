83d2388b8b8ae66b0536f206382eafb4157e95fa
Sebastian <sebastian.kosciolek2000@gmail.com>
2025-03-19 18:48:51 +0100

    feat: configure nativeWind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+
+import "./globals.css";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -29,10 +31,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <Tabs>
+        <Tabs.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Tabs.Screen name="+not-found" />
+      </Tabs>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -3,22 +3,25 @@ import { openBrowserAsync } from 'expo-web-browser';
 import { type ComponentProps } from 'react';
 import { Platform } from 'react-native';
 
-type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
+type RelativePathString = string; // Define as needed
+type ExternalPathString = string; // Define as needed
+
+type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: RelativePathString | ExternalPathString | "/sign-in" | `/sign-in?${string}` | `/sign-in#${string}` | "/_sitemap" | `/_sitemap?${string}` | `/_sitemap#${string}` | "/(root)/(tabs)/explore" | string };
 
 export function ExternalLink({ href, ...rest }: Props) {
-  return (
-    <Link
-      target="_blank"
-      {...rest}
-      href={href}
-      onPress={async (event) => {
-        if (Platform.OS !== 'web') {
-          // Prevent the default behavior of linking to the default browser on native.
-          event.preventDefault();
-          // Open the link in an in-app browser.
-          await openBrowserAsync(href);
-        }
-      }}
-    />
-  );
+    return (
+        <Link
+            target="_blank"
+            {...rest}
+            href={href}
+            onPress={async (event) => {
+                if (Platform.OS !== 'web') {
+                    // Prevent the default behavior of linking to the default browser on native.
+                    event.preventDefault();
+                    // Open the link in an in-app browser.
+                    await openBrowserAsync(href);
+                }
+            }}
+        />
+    );
 }
