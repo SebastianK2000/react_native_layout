ca3750fa9709ae4eb115fd669beb415a02706cf6
Sebastian <sebastian.kosciolek2000@gmail.com>
2025-03-19 19:52:34 +0100

    feat: configure nativeWind update

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,7 @@
-import {  View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
+
+import "../../globals.css";
 
 export default function Index() {
   return (
@@ -11,10 +13,12 @@ export default function Index() {
       }}
     >
 
+        <Text className="font-bold text-lg my-10 ">Welcome to ReState</Text>
+
       <Link href="../../sign-in.tsx">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
-      <Link href="../properties/1">Property</Link>
+      <Link href="../properties">Property</Link>
 
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,41 +1,7 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Tabs } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
 
 import "./globals.css";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Tabs>
-        <Tabs.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Tabs.Screen name="+not-found" />
-      </Tabs>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
